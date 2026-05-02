b1d1b59b393a7e68c1f770dee297a81e278fb265
tidionata <tidionata@gmail.com>
2026-05-01 21:19:49 -0400

    fix: adiciona import do Separator em SettingsPage (causava tela branca)

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Separator } from "@/components/ui/separator";
 import { toast } from "sonner";
 import {
   Link2, Store, Copy, ExternalLink, ShoppingCart,
